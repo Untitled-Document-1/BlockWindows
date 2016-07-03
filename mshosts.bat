ab9a8411b09258e8b475e2cbdad09534e869c6fe
Untitled-Document-1 <jadamson60@yahoo.co.uk>
2016-07-03 13:53:01 +0100

    Replacement batch and list files for blocking MS hosts

---
+++ mshosts.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+REM Blocks and unblocks Microsoft servers, including Outlook, Hotmail, Live.com, Bing.com
+CHOICE /C BU /M "Type [B] to block Microsoft hosts or [U] to unblock Microsoft hosts" /N
+IF %errorlevel%==1 SET _CHOICE=Blocking& SET _ROUTECMD=route -p add
+IF %errorlevel%==2 SET _CHOICE=Unblocking& SET _ROUTECMD=route -p delete
+FOR /F "tokens=1,2 delims=," %%A IN (mshosts.list) DO (
+ECHO. & ECHO %_CHOICE% %%A
+%_ROUTECMD% %%B/32 127.0.0.1
+)
