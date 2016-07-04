57fc6c2ca820e63a178900c4b0901d9336131592
Untitled-Document-1 <jadamson60@yahoo.co.uk>
2016-07-04 20:16:14 +0100

    Succintified the Admin rights check

--- hosts.bat
+++ hosts.bat
@@ -3,11 +3,7 @@ REM This blocks MS telemetry IP's from hosts file. Appends to current hosts file
 REM WIN10 blocks this method, if running WIN10 add the entries in the file 'hosts' to your router or firewall blocklist
 
 openfiles.exe 1>nul 2>&1
-if not %errorlevel% equ 0 (
-    Echo Requires Administrator Privileges! You are not administrator! Right Click file select run as admin
-    pause
-    EXIT 1
-) else (
+if errorlevel 1 ECHO You need to run this script from an elevated command prompt. Exiting. && EXIT /B 1
 
 type %~dp0hosts.list >> %windir%\system32\Drivers\etc\hosts
 echo Completed
